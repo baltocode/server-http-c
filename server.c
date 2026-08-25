36f7a7bf0b1e515acecb482481a7fbae504ada08
Donato Roggero <donato.roggero@gmail.com>
2026-08-25 20:45:44 +0200

    Bind server socket to port 8080

--- server.c
+++ server.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
+
+#define PORT 8080
 
 int main(void)
 {
     int server_socket;
+    struct sockaddr_in server_address = {0};
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -13,7 +17,21 @@ int main(void)
         return 1;
     }
 
-    printf("Socket created successfully: %d\n", server_socket);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+
+    if (bind(
+            server_socket,
+            (struct sockaddr *)&server_address,
+            sizeof(server_address)
+        ) == -1) {
+        perror("bind");
+        close(server_socket);
+        return 1;
+    }
+
+    printf("Socket bound to port %d\n", PORT);
 
     close(server_socket);
 
